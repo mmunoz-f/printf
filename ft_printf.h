7ded15979ec62dc8b13ba5598f49809da6bb91b1
Miguel angel Munoz fernandez <mmunoz-f@c1r13s6.42madrid.com>
2021-02-12 18:26:11 +0100

    Primera hora

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmunoz-f <mmunoz-f@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/12 15:34:07 by mmunoz-f          #+#    #+#             */
+/*   Updated: 2021/02/12 15:39:19 by mmunoz-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Que es el flag '0': %-f\n", 577.3);
+	return (0);
+}
